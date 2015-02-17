cfe337576772149cda94d524bd9f268033080cc1
rahulprasad5 <rahul.grb@gmail.com>
2015-02-17 22:21:37 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*COPYRIGHT*/
+/*You are welcome to use the code for knowledge, If you are using it on your code, send a shout out to my name */
+/*Enjoy the Game! :) */
+
+#include "game.h"
+
+int main( int argc, char** argv )
+{
+	Game g;
+	g.Start();
+
+	return 0;
+}
